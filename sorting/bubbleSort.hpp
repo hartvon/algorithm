48d72b9efd903591cb1a0c38081d9955beb9ae6c
hotfeng <66011700+hotfeng@users.noreply.github.com>
2022-08-22 14:20:42 +0800

    Update bubbleSort.hpp

--- sorting/bubbleSort.hpp
+++ sorting/bubbleSort.hpp
@@ -3,13 +3,13 @@
 
 #include <vector>
 
-void bubbleSort(std::vector<int>& strs, int n) {
+void bubbleSort(std::vector<int>& nums, int n) {
     bool sorted = false;
     while (!sorted) {
         sorted = true;
         for (int i = 1; i < n; ++i) {
-            if (strs[i-1] + strs[i] > strs[i] + strs[i-1]) {
-                std::swap(strs[i-1], strs[i]);
+            if (nums[i-1] > nums[i]) {
+                std::swap(nums[i-1], nums[i]);
                 sorted = false;
             }
         }
